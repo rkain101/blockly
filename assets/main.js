391b35a75eae82bef341518c61885308e706aed1
Ragav Kumar <27591832+rkain101@users.noreply.github.com>
2018-06-27 09:29:09 -0700

    closure library + full workspace

--- assets/main.js
+++ assets/main.js
@@ -3,9 +3,57 @@ $("#blocklyArea").css("height", "calc(100vh - " + $(".top-matter").css("height")
 
 var blocklyArea = document.getElementById('blocklyArea');
 var blocklyDiv = document.getElementById('blocklyDiv');
-var workspace = Blockly.inject(blocklyDiv,
+
+//--------------------Imported
+/* TODO: Change toolbox XML ID if necessary. Can export toolbox XML from Workspace Factory. */
+var toolbox = document.getElementById("toolbox");
+
+var options = { 
+	toolbox : toolbox, 
+	collapse : true, 
+	comments : true, 
+	disable : true, 
+	maxBlocks : Infinity, 
+	trashcan : true, 
+	horizontalLayout : false, 
+	toolboxPosition : 'start', 
+	css : true, 
+	media : 'vendor/media/', 
+	rtl : false, 
+	scrollbars : true, 
+	sounds : true, 
+	oneBasedIndex : true, 
+	grid : {
+		spacing : 20, 
+		length : 1, 
+		colour : '#888', 
+		snap : true
+	}, 
+	zoom : {
+		controls : true, 
+		wheel : true, 
+		startScale : 1, 
+		maxScale : 3, 
+		minScale : 1, 
+		scaleSpeed : 1
+	}
+};
+
+/* Inject your workspace */ 
+var workspace = Blockly.inject(blocklyDiv, options);
+
+/* Load Workspace Blocks from XML to workspace. Remove all code below if no blocks to load */
+
+/* TODO: Change workspace blocks XML ID if necessary. Can export workspace blocks XML from Workspace Factory. */
+var workspaceBlocks = document.getElementById("workspaceBlocks"); 
+
+/* Load blocks to workspace. */
+Blockly.Xml.domToWorkspace(workspaceBlocks, workspace);
+//--------------------End Import
+
+/*var workspace = Blockly.inject(blocklyDiv,
 		{media: 'vendor/media/',
-		 toolbox: document.getElementById('toolbox')});
+		 toolbox: document.getElementById('toolbox')});*/
 var onresize = function(e) {
 	// Compute the absolute coordinates and dimensions of blocklyArea.
 	var element = blocklyArea;
